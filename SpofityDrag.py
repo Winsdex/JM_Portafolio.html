ca77db84a3c6f0699e23a39562a60bb4dffdcfa1
Winsdex <144508554+Winsdex@users.noreply.github.com>
2023-09-09 00:02:03 -0600

    Add files via upload

---
+++ SpofityDrag.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  9 00:01:12 2023
+
+@author: sir_j
+"""
+
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+
+from spotify_etl import run_spotify_etl
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 11, 8),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'spotify_dag',
+    default_args=default_args,
+    description='Our first DAG with ETL process!',
+    schedule_interval=timedelta(days=1),
+)
+
+def just_a_function():
+    print("I'm going to show you something :)")
+
+run_etl = PythonOperator(
+    task_id='whole_spotify_etl',
+    python_callable=run_spotify_etl,
+    dag=dag,
+)
+
+run_etl
